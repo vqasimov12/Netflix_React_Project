964ad5f69965785d0b0a9f80af17dd242f2f9791
Qasimov Vaqif Xalid <Qasim_wu20@itstep.edu.az>
2024-12-16 11:49:53 +0400

    Project Start

---
+++ src/homepage/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div className="flex items-center border border-transparent border-t-[4rem] border-t-transparent rounded-tl-[50%_100%] rounded-tr-[50%_100%] bg-gradient-radial from-[#4061e7] via-transparent to-[#0000001a]">
+      
+    </div>
+  )
+}
+
+export default Homepage
