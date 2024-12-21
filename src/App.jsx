14787b52807c6bccd0bf06abb45c3263d8c99e75
vqasimov12 <q.vaqif1211@gmail.com>
2024-12-21 15:06:34 +0400

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Homepage from "./homepage/Homepage"
+import Landingpage from "./landingpage/Landingpage"
 function App() {
 
   return (
     <>
-    <Homepage/>
+    <Landingpage/>
     </>
   )
 }
---
+++ src/landingpage/Landingpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Landingpage = () => {
+  return (
+   <>
+   
+   </>
+  )
+}
+
+export default Landingpage
